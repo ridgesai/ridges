cb00def1d84351180dead8536000c7ac104501e1
Shakeel Hussein <shakeel.hussein@mail.utoronto.ca>
2025-09-12 19:33:29 -0400

    fill out

--- new_validator/main.py
+++ new_validator/main.py
@@ -1,1 +1,46 @@
 # Handles validator start and stop, including creating websocket connection, cleanup ops and version checks before anything happens, etc
+from typing import Optional, Any
+from enum import Enum
+
+import websockets
+
+import asyncio
+class ValidatorStatus(Enum):
+    STARTING = 'starting'
+    IDLING = 'idling'
+    EVALUATING = 'evaluating'
+    SHUTTING_DOWN = 'shutting_down'
+
+class RidgesValidator:
+    socket_manager: Optional[Any] # TODO: typing for socket manager too
+    sandbox_manager: Optional[Any] # TODO: Add typing once sandbox mgr is created
+    proxy_manager: Optional[Any]
+    status: ValidatorStatus
+
+    def __init__(self) -> None:
+        self.ws = None
+        self.sandbox_manager = None
+        self.status = ValidatorStatus.STARTING
+
+    async def start(self):
+        pass
+
+    async def shutdown(self):
+        pass
+
+async def main():
+    """
+    This starts up the validator websocket, which connects to the Ridges platform 
+    It receives and sends events like new agents to evaluate, eval status, scores, etc
+    """
+    validator = RidgesValidator()
+    try:
+        await validator.start()
+    except KeyboardInterrupt:
+        await validator.shutdown()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
--- new_validator/socket.py
+++ new_validator/socket.py
@@ -1,6 +1,47 @@
 # All communications between validator and platform 
 
-# Heartbeat
-# Start eval
-# Post results for eval run
-# Weights
+from typing import Dict, Any
+
+import websockets 
+from typing import Any, Optional
+
+# TODO: Common message protocol structure?
+
+class SocketManager:
+    ws: Optional[websockets.ClientConnection]
+
+    def __init__(self) -> None:
+        self.ws = None
+
+    async def create_connection(self):
+        pass
+
+    async def close_connection(self):
+        pass
+    
+    async def send(self, message: Dict[str, Any]): 
+        pass
+
+    async def handle_message():
+        event = ""
+
+        match event:
+            case "evaluation":
+                pass
+
+            case "set-weights":
+                pass
+
+            case "authentication-failed":
+                raise SystemExit(f"FATAL: TODO")
+        # Heartbeat
+        # Start eval
+        # Post results for eval run
+        # Weights
+        pass
+
+class SocketManager():
+    pass
+
+    async def send():
+        pass
