2b583f6dccecaf7fa70464b0b371c5e4a1ddc3ba
Stephen Guo <stephen@ridges.ai>
2025-11-13 12:22:56 -0500

    Include validator hotkeys for mining

--- api/src/utils/refresh_subnet_hotkeys.py
+++ api/src/utils/refresh_subnet_hotkeys.py
@@ -6,5 +6,5 @@ from bittensor.core.subtensor import Subtensor
 def check_if_hotkey_is_registered(hotkey: str) -> bool:
     subtensor = Subtensor(network=config.SUBTENSOR_NETWORK)
     metagraph = Metagraph(netuid=config.NETUID, network=config.SUBTENSOR_NETWORK, sync=True, subtensor=subtensor)
-    registered_hotkeys = {neuron.hotkey for neuron in metagraph.neurons if not neuron.validator_permit}
+    registered_hotkeys = {neuron.hotkey for neuron in metagraph.neurons}
     return hotkey in registered_hotkeys
