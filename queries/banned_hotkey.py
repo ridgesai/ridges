d1caf21b6e77f5ac1704de9bbecaaf21dc08f0d5
adamridges <adam@hiddenharvestventures.com>
2025-11-14 15:48:00 -0500

    cleanup style

--- queries/banned_hotkey.py
+++ queries/banned_hotkey.py
@@ -3,13 +3,18 @@ from typing import Optional
 from models.banned_hotkey import BannedHotkey
 from utils.database import db_operation, DatabaseConnection
 
+
+
 @db_operation
 async def get_banned_hotkey(conn: DatabaseConnection, miner_hotkey: str) -> Optional[BannedHotkey]:
-    banned_hotkey = await conn.fetchrow("""
-    SELECT * FROM banned_hotkeys WHERE miner_hotkey = $1
-    """, miner_hotkey)
+    banned_hotkey = await conn.fetchrow(
+        """
+        SELECT * FROM banned_hotkeys WHERE miner_hotkey = $1
+        """,
+        miner_hotkey
+    )
 
-    if banned_hotkey:
-        return BannedHotkey(**banned_hotkey)
+    if not banned_hotkey:
+        return None
 
-    return None
+    return BannedHotkey(**banned_hotkey)
