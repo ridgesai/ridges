35810ffc8f0a8f38195e61c11025c0fee504f7ac
Adam Sidat <adam@hiddenharvestventures.com>
2025-11-18 12:26:19 -0500

    chutes updated their embedding url

--- inference_gateway/providers/chutes.py
+++ inference_gateway/providers/chutes.py
@@ -13,7 +13,7 @@ from inference_gateway.models import InferenceTool, EmbeddingResult, InferenceRe
 
 if config.USE_CHUTES:
     CHUTES_INFERENCE_MODELS_URL = f"{config.CHUTES_BASE_URL}/models" # https://llm.chutes.ai/v1/models
-    CHUTES_EMBEDDING_MODELS_URL = "https://llm.chutes.ai/chutes/?template=embedding" # TODO ADAM
+    CHUTES_EMBEDDING_MODELS_URL = "https://api.chutes.ai/chutes/?template=embedding" # TODO ADAM
 
 
 
