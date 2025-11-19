bd76de7e4866037f7ab8ce1b7d24daf82ca9a5d7
Adam Sidat <adam@hiddenharvestventures.com>
2025-11-19 17:31:00 -0500

    renamed dataset and added python integration

---
+++ evaluator/datasets/problem_statistics.py
@@ -0,0 +1,30 @@
+import json
+import pathlib
+
+from enum import Enum
+from typing import Tuple, Optional
+
+
+
+class ProblemStatisticsProblemSuite(str, Enum):
+    swebench_verified = "swebench_verified"
+    polyglot = "polyglot"
+
+class ProblemStatisticsProblemDifficulty(str, Enum):
+    easy = "easy"
+    medium = "medium"
+    hard = "hard"
+
+
+
+with open(pathlib.Path(__file__).parent / "problem_statistics.json", "r") as f:
+    problem_statistics_json = json.load(f)
+
+
+
+def get_problem_statistics_by_problem_name(problem_name: str) -> Optional[Tuple[ProblemStatisticsProblemSuite, ProblemStatisticsProblemDifficulty]]:
+    if not problem_name in problem_statistics_json:
+        return None
+
+    problem_statistics = problem_statistics_json[problem_name]
+    return problem_statistics["problem_suite"], problem_statistics["problem_difficulty"]
