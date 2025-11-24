700e53f912e39e34d70e1a555e105e8ba0ba0a53
Stephen Guo <stephen@ridges.ai>
2025-11-24 16:17:45 -0500

    Temp fix set weights

--- utils/bittensor.py
+++ utils/bittensor.py
@@ -1,11 +1,12 @@
+from bittensor.core.async_subtensor import AsyncSubtensor
 import api.config as config
 import utils.logger as logger
-
+import base58
 from bittensor_wallet.keypair import Keypair
-from bittensor.core.async_subtensor import AsyncSubtensor
 
 
 
+VALID_HOTKEY_LENGTH = 48
 subtensor = AsyncSubtensor(network=config.SUBTENSOR_NETWORK)
 
 
@@ -21,4 +22,13 @@ def validate_signed_timestamp(timestamp: int, signed_timestamp: str, hotkey: str
         return keypair.verify(str(timestamp), bytes.fromhex(signed_timestamp))
     except Exception as e:
         logger.warning(f"Error in validate_signed_timestamp(timestamp={timestamp}, signed_timestamp={signed_timestamp}, hotkey={hotkey}): {e}")
+        return False
+
+
+
+def is_valid_hotkey_string(hotkey: str) -> bool:
+    try:
+        base58.b58decode(hotkey)
+        return len(hotkey) == VALID_HOTKEY_LENGTH
+    except ValueError:
         return False
--- validator/set_weights.py
+++ validator/set_weights.py
@@ -1,14 +1,11 @@
+from bittensor.core.async_subtensor import AsyncSubtensor
+from utils.bittensor import check_if_hotkey_is_registered, is_valid_hotkey_string
 import utils.logger as logger
 import validator.config as config
 
 from typing import Dict
-from bittensor.core.async_subtensor import AsyncSubtensor
-
-
 
 subtensor = AsyncSubtensor(network=config.SUBTENSOR_NETWORK, fallback_endpoints=[config.SUBTENSOR_ADDRESS])
-
-
 
 async def set_weights_from_mapping(weights_mapping: Dict[str, float]) -> None:
     if len(weights_mapping.keys()) != 1:
@@ -18,6 +15,14 @@ async def set_weights_from_mapping(weights_mapping: Dict[str, float]) -> None:
     weight_receiving_hotkey = list(weights_mapping.keys())[0]
     if weights_mapping[weight_receiving_hotkey] != 1:
         logger.error("Expected weight of 1")
+        return
+
+    if not is_valid_hotkey_string(weight_receiving_hotkey):
+        logger.error(f"Weight receiving hotkey {weight_receiving_hotkey} is not a valid Base58 encoded string")
+        return
+
+    if not await check_if_hotkey_is_registered(weight_receiving_hotkey):
+        logger.error(f"Weight receiving hotkey {weight_receiving_hotkey} not registered")
         return
 
     weight_receiving_uid = await subtensor.get_uid_for_hotkey_on_subnet(hotkey_ss58=weight_receiving_hotkey, netuid=config.NETUID)
