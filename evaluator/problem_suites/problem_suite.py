7b99655039bea3c64e8c873ac3dfd1b1e5c4b89e
Adam Sidat <adam@hiddenharvestventures.com>
2025-11-26 13:59:02 -0500

    changed abc interface for problem suites

--- evaluator/problem_suites/problem_suite.py
+++ evaluator/problem_suites/problem_suite.py
@@ -17,54 +17,18 @@ from evaluator.sandbox.sandbox_manager import Sandbox, SandboxManager
 
 
 class ProblemSuite(ABC):
-    """
-    Abstract base class for a problem suite.
-    Classes like PolyglotProblemSuite and SWEBenchProblemSuite inherit from this class.
-    """
-
-
-
-    def __init__(self, dataset_path: str):
-        self.problems = {}
-        
-        self.dataset_path = dataset_path
-        self._load_problems(dataset_path)
-
-
-
-    def _add_problem(self, problem: Problem) -> None:
-        """
-        Adds a problem to the problem suite.
-        Should only be called from within the load_problems() implementation of a derived class.
-        """
-        
+    def _add_problem(self, problem: Problem) -> None:     
         if problem.name in self.problems:
-            logger.fatal(f"Problem {problem.name} already exists in the suite")
+            logger.fatal(f"Problem {problem.name} already exists")
         
         self.problems[problem.name] = problem
 
-    @abstractmethod
-    def _load_problems(self, dataset_path: str):
-        """
-        Loads all the problems from the given dataset path.
-        Each inherited class must implement this method according to how their problem suite is structured.
-        The implementation should call the _add_problem() method as many times as needed.
-        """
 
-        pass
 
     def has_problem_name(self, problem_name: str) -> bool:
-        """
-        Returns True if the problem suite has a problem with the given name.
-        """
-        
         return problem_name in self.problems
 
-    def get_problem(self, problem_name: str) -> Problem:
-        """
-        Returns the problem with the given name.
-        """
-        
+    def get_problem(self, problem_name: str) -> Problem:  
         return self.problems.get(problem_name)
 
 
@@ -77,11 +41,6 @@ class ProblemSuite(ABC):
         *,
         include_tests: bool = False
     ) -> None:
-        """
-        Copies the problem files to the given directory.
-        Each inherited class must implement this method according to how their problem suite is structured.
-        """
-        
         pass
 
 
