cf1c97c239b9286b293d27cf4a6a05952c84710f
Stephen Guo <stephen@ridges.ai>
2025-12-12 22:46:47 -0500

    Re-fix tests solutions for unpatched Polyglots

--- evaluator/datasets/polyglot_py_unpatched/error-handling/tests.py
+++ evaluator/datasets/polyglot_py_unpatched/error-handling/tests.py
@@ -1,8 +1,33 @@
 import unittest
 
 import main as er
-from test_utils import FileLike
 
+class FileLike:
+    def __init__(self, fail_something=True):
+        self.is_open = False
+        self.was_open = False
+        self.did_something = False
+        self.fail_something = fail_something
+
+    def open(self):
+        self.was_open = False
+        self.is_open = True
+
+    def close(self):
+        self.is_open = False
+        self.was_open = True
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+    def do_something(self):
+        self.did_something = True
+        if self.fail_something:
+            raise Exception("Failed while doing something")
 
 class ErrorHandlingTest(unittest.TestCase):
     def test_throw_exception(self):
