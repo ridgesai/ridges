c1b0f5e9171418fef4f299289fff610143483ea3
adamridges <adam@hiddenharvestventures.com>
2025-12-15 18:01:03 -0500

    clean

--- api/endpoints/evaluation_sets.py
+++ api/endpoints/evaluation_sets.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import APIRouter
 from models.evaluation_set import EvaluationSetProblem
-from queries.evaluation_set import get_all_evaluation_set_problems_for_set_id, get_latest_set_id
+from queries.evaluation_set import get_latest_set_id, get_all_evaluation_set_problems_for_set_id
 
 
 
@@ -12,5 +12,4 @@ router = APIRouter()
 # /evaluation-sets/all-latest-set-problems
 @router.get("/all-latest-set-problems")
 async def evaluation_sets_all_latest_set_problems() -> List[EvaluationSetProblem]:
-    max_problem_set_id = await get_latest_set_id()
-    return await get_all_evaluation_set_problems_for_set_id(max_problem_set_id)
+    return await get_all_evaluation_set_problems_for_set_id(await get_latest_set_id())
