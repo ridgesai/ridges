ed6a5b9f0953116ea7300dbc82c47041b847e6f5
Stephen Guo <stephen@ridges.ai>
2025-12-15 14:32:48 -0500

    Try fixing paasio-py types

--- evaluator/datasets/polyglot_py/paasio/main.py
+++ evaluator/datasets/polyglot_py/paasio/main.py
@@ -1,77 +1,78 @@
 import io
+from typing import Any, Optional
 
 
 class MeteredFile(io.BufferedRandom):
     """Implement using a subclassing model."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         pass
 
-    def __enter__(self):
+    def __enter__(self) -> "MeteredFile":
         pass
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Optional[bool]:
         pass
 
-    def __iter__(self):
+    def __iter__(self) -> "MeteredFile":
         pass
 
-    def __next__(self):
+    def __next__(self) -> bytes:
         pass
 
-    def read(self, size=-1):
-        pass
-
-    @property
-    def read_bytes(self):
+    def read(self, size: int = -1) -> bytes:
         pass
 
     @property
-    def read_ops(self):
-        pass
-
-    def write(self, b):
+    def read_bytes(self) -> int:
         pass
 
     @property
-    def write_bytes(self):
+    def read_ops(self) -> int:
+        pass
+
+    def write(self, b: bytes) -> int:
         pass
 
     @property
-    def write_ops(self):
+    def write_bytes(self) -> int:
+        pass
+
+    @property
+    def write_ops(self) -> int:
         pass
 
 
 class MeteredSocket:
     """Implement using a delegation model."""
 
-    def __init__(self, socket):
+    def __init__(self, socket: Any) -> None:
         pass
 
-    def __enter__(self):
+    def __enter__(self) -> "MeteredSocket":
         pass
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Optional[bool]:
         pass
 
-    def recv(self, bufsize, flags=0):
-        pass
-
-    @property
-    def recv_bytes(self):
+    def recv(self, bufsize: int, flags: int = 0) -> bytes:
         pass
 
     @property
-    def recv_ops(self):
-        pass
-
-    def send(self, data, flags=0):
+    def recv_bytes(self) -> int:
         pass
 
     @property
-    def send_bytes(self):
+    def recv_ops(self) -> int:
+        pass
+
+    def send(self, data: bytes, flags: int = 0) -> int:
         pass
 
     @property
-    def send_ops(self):
+    def send_bytes(self) -> int:
+        pass
+
+    @property
+    def send_ops(self) -> int:
         pass
