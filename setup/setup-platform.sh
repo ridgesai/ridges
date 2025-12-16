8b408bf8871e1eee6d942357b1dfeadcc1784785
Stephen Guo <stephen@ridges.ai>
2025-12-15 19:00:52 -0500

    Update python to 3.14

--- setup/setup-platform.sh
+++ setup/setup-platform.sh
@@ -23,11 +23,9 @@ echo 'export PATH="/home/ubuntu/.local/bin:$PATH"' >> /home/ubuntu/.bashrc
 git clone https://github.com/ridgesai/ridges.git ~/ridges
 cd ~/ridges
 
-uv venv --python 3.11
+uv venv --python 3.13
 source .venv/bin/activate
 uv pip install .
 
-pm2 start 'uv run -m api.main' --name ridges-platform
+pm2 start 'uv run -m api.src.main' --name ridges-platform
 pm2 stop ridges-platform
-
-exec sudo --user "$USER" bash -c "cd ~/ridges && source .venv/bin/activate && exec bash -l"
