a26147e3aaaa017a88428aa441e5479350449ef4
Cameron Fairchild <c@latent.to>
2026-02-13 11:58:34 -0500

    remove old glm

--- inference_gateway/providers/chutes.py
+++ inference_gateway/providers/chutes.py
@@ -29,7 +29,6 @@ class WhitelistedChutesModel(BaseModel):
 if config.USE_CHUTES:
     WHITELISTED_CHUTES_INFERENCE_MODELS = [
         WhitelistedChutesModel(name="Qwen/Qwen3-Coder-480B-A35B-Instruct-FP8", chutes_name="Qwen/Qwen3-Coder-480B-A35B-Instruct-FP8-TEE"),
-        WhitelistedChutesModel(name="zai-org/GLM-4.5-FP8", chutes_name="zai-org/GLM-4.5"),
         WhitelistedChutesModel(name="deepseek-ai/DeepSeek-V3-0324", chutes_name="deepseek-ai/DeepSeek-V3-0324-TEE"),
         WhitelistedChutesModel(name="moonshotai/Kimi-K2-Instruct", chutes_name="moonshotai/Kimi-K2-Instruct-0905"),
         WhitelistedChutesModel(name="zai-org/GLM-4.6-FP8", chutes_name="zai-org/GLM-4.6-TEE"),
