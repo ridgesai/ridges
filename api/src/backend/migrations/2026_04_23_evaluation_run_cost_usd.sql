86477adf0f4c4fd7b7f8516b1429324e90ce2ef2
ibraheem-latent <i@latent.to>
2026-05-05 14:58:17 -0700

    remove old migrations

--- api/src/backend/migrations/2026_04_23_evaluation_run_cost_usd.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE evaluation_runs ADD COLUMN IF NOT EXISTS cost_usd DOUBLE PRECISION;
-
-CREATE OR REPLACE VIEW evaluation_runs_with_cost AS
-SELECT
-    er.*,
-    COALESCE(er.cost_usd, 0) AS total_cost_usd
-FROM evaluation_runs er;
