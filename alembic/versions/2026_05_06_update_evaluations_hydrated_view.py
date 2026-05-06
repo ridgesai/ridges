29345cd6442e4176fb973cd52931ad7567fb6c88
jmnmv12 <j.vasconcelos99@gmail.com>
2026-05-06 11:24:22 +0100

    refactor: :card_file_box: Create alembic migration for evaluations_hydrated view updates

---
+++ alembic/versions/2026_05_06_update_evaluations_hydrated_view.py
@@ -0,0 +1,58 @@
+"""Update evaluations hydrated view
+
+Revision ID: 234ed0606f2a
+Revises: b8d3f6e2c9a5
+Create Date: 2026-05-06 11:03:17.836098
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "234ed0606f2a"
+down_revision: Union[str, Sequence[str], None] = "b8d3f6e2c9a5"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        CREATE OR REPLACE VIEW evaluations_hydrated AS
+        SELECT
+            evaluations.*,
+            (CASE
+                 WHEN EVERY(erh.status = 'finished' OR (erh.status = 'error' AND erh.error_code BETWEEN 1000 AND 1999)) THEN 'success'
+                 WHEN EVERY(erh.status IN ('finished', 'error')) THEN 'failure'
+                 ELSE 'running'
+                END)::evaluationstatus AS status,
+            COUNT(*) FILTER (WHERE erh.solved)::float / COUNT(*) AS score,
+            AVG(
+                EXTRACT(EPOCH FROM (erh.finished_or_errored_at - erh.started_running_agent_at))
+            ) FILTER (WHERE erh.solved) AS avg_running_secs
+        FROM evaluations
+            INNER JOIN evaluation_runs_hydrated erh USING (evaluation_id)
+        GROUP BY evaluations.evaluation_id;
+    """
+    )
+
+
+def downgrade() -> None:
+    op.execute(
+        """
+        CREATE OR REPLACE VIEW evaluations_hydrated AS
+        SELECT
+            evaluations.*,
+            (CASE
+                 WHEN EVERY(erh.status = 'finished' OR (erh.status = 'error' AND erh.error_code BETWEEN 1000 AND 1999)) THEN 'success'
+                 WHEN EVERY(erh.status IN ('finished', 'error')) THEN 'failure'
+                 ELSE 'running'
+                END)::evaluationstatus AS status,
+            COUNT(*) FILTER (WHERE erh.solved)::float / COUNT(*) AS score
+        FROM evaluations
+            INNER JOIN evaluation_runs_hydrated erh USING (evaluation_id)
+        GROUP BY evaluations.evaluation_id;
+    """
+    )
