b1724bc781c2a6e803e7a63a4f54179833be6933
João Vasconcelos <37747816+jmnmv12@users.noreply.github.com>
2026-05-11 16:26:25 +0100

    Feat: Add extrinsic index to refunds table (#365)
    * feat: :card_file_box: Add `payment_extrinsic_index` to refunds table
    
    * fix: :card_file_box: Add extrinsic index for refund transaction and upload transaction
    
    * fix: :card_file_box: Fix migration head conflict

--- db/models/refund.py
+++ db/models/refund.py
@@ -16,15 +16,51 @@ class FailedUploadRefund(Base, CreatedAtMixin):
         server_default=sa.text("gen_random_uuid()"),
         nullable=False,
     )
-    block_hash: Mapped[str] = mapped_column(sa.Text, nullable=False)
-    amount: Mapped[int] = mapped_column(sa.BigInteger, nullable=False)
-    tx_hash: Mapped[str] = mapped_column(sa.Text, nullable=False)
-    upload_tx_hash: Mapped[str] = mapped_column(sa.Text, nullable=False)
-    upload_block_hash: Mapped[str] = mapped_column(sa.Text, nullable=False)
+    # Refund transaction details
+    tx_hash: Mapped[str] = mapped_column(
+        sa.Text,
+        nullable=False,
+        comment="The transaction hash of the refund transaction",
+    )
+    block_hash: Mapped[str] = mapped_column(
+        sa.Text,
+        nullable=False,
+        comment="The block hash of the refund transaction",
+    )
+    block_extrinsic_index: Mapped[str] = mapped_column(
+        sa.Text,
+        nullable=False,
+        comment="The block extrinsic index of the transaction associated with the agent upload that failed and caused the refund",
+    )
+    amount: Mapped[int] = mapped_column(sa.BigInteger, nullable=False, comment="The amount to refund in RAO")
+    # Upload transaction details
+    upload_tx_hash: Mapped[str] = mapped_column(
+        sa.Text,
+        nullable=False,
+        comment="The transaction hash of the transaction associated with the agent upload that failed and caused the refund",
+    )
+    upload_block_hash: Mapped[str] = mapped_column(
+        sa.Text,
+        nullable=False,
+        comment="The block hash of the transaction associated with the agent upload that failed and caused the refund",
+    )
+    upload_block_extrinsic_index: Mapped[str] = mapped_column(
+        sa.Text,
+        nullable=False,
+        comment="The block extrinsic index of the transaction associated with the agent upload that failed and caused the refund",
+    )
+    upload_amount: Mapped[int] = mapped_column(
+        sa.BigInteger,
+        nullable=False,
+        comment="The transfer amount in RAO of the transaction associated with the agent upload that failed and caused the refund",
+    )
+    coldkey: Mapped[str] = mapped_column(
+        sa.Text,
+        nullable=False,
+        comment="The coldkey of the miner for which the upload failed and caused the refund",
+    )
     updated_at: Mapped[sa.DateTime] = mapped_column(
         sa.TIMESTAMP(timezone=True),
         nullable=False,
         server_default=sa.text("NOW()"),
     )
-    coldkey: Mapped[str] = mapped_column(sa.Text, nullable=False)
-    upload_amount: Mapped[int] = mapped_column(sa.BigInteger, nullable=False)
